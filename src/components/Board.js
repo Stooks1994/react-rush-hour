d37bb6f752dbb8a8c2c0de8d5a5b63e59004d0d2
Ian <ian@Ians-MacBook-Air.local>
2021-08-09 13:00:37 -0400

    working on bounds

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Piece from "./Piece";
-import BoardRow from "./BoardRow"
 import '../styling/Board.css'
 
 const BOARD_SIZE = 6;
@@ -13,97 +12,45 @@ const TEST_PUZZLE = {
     ]
 };
 
-const initializeBoardFromPuzzle = (puzzle) => {
-    let board = []
-
-    for (let cols = 0; cols < BOARD_SIZE; cols++) {
-        board.push([]);
-
-        for (let rows = 0; rows < BOARD_SIZE; rows++) {
-            board[cols].push({x:cols, y:rows, isOccupied:false, id:null});
-        }
-    }
-
-    const markAsOccupied = (board, id, x, y, size, orientation) => {
-        if (orientation == 'HORIZONTAL') {
-            for (let i = 0; i < size; i++) {
-                board[x + i][y].isOccupied = true;
-                board[x + i][y].id = id;
-            }
-        } else {
-            for (let j = 0; j < size; j++) {
-                board[x][y + j].isOccupied = true;
-                board[x][y + j].id = id;
-            }
-        }
-    }
-
-    puzzle.pieces.map(piece => markAsOccupied(board, piece.id, piece.x, piece.y, piece.size, piece.orientation))
-
-    return board
-}
-
 const Board = () => {
-    const populateBoardFromPuzzle = (puzzle, occupiedSpaces) => {
-        return puzzle.pieces.map(piece => <Piece key={piece.id} pieceProps={piece} occupied={occupiedSpaces} updateCoords={updateOccupiedSpaces} />)
+    const [isMounted, setIsMounted] = useState(false);
+
+    const populateBoardFromPuzzle = puzzlePieces => {
+        return puzzlePieces.map(piece => (
+            <Piece 
+                key={piece.id} 
+                pieceProps={piece} 
+                allPieces={puzzlePieces} 
+                onFinishedDragging={updatePiecesOnBoard} 
+            />)
+        )
     }
 
-    const populatePiecesFromOccupiedSpaces = (puzzle, occupiedSpaces) => {
-
-    }
-
-    const updateOccupiedSpaces = (id, x, y, orientation, size) => {
-        /*
-        const markAsUnoccupied = (element, targetId) => {
-            if (element.id == targetId) {
-                element.isOccupied = false;
-                element.id = null;
-            }
-        }
-
-        setOccupiedSpaces((prevState) => {
+    const updatePiecesOnBoard = (id, newBin, orientation) => {
+        setPiecesOnBoard((prevState) => {
             let newState = prevState;
 
-            newState.forEach(row => row.forEach(element => markAsUnoccupied(element, id)))
-    
-            if (orientation == 'HORIZONTAL') {
-                if (x + size <= BOARD_SIZE) {
-                    for (let i = 1; i < size; i++) {
-                        newState[BOARD_SIZE - i][y].isOccupied = true;
-                        newState[BOARD_SIZE - i][y].id = id;
-                    }
-                } else {
-                    for (let i = 0; i < size; i++) {
-                        newState[x + i][y].isOccupied = true;
-                        newState[x + i][y].id = id;
+            newState.forEach(element => {
+                if (element.id === id) {
+                    if (orientation == 'HORIZONTAL') {
+                        element.x = newBin;
+                    } else {
+                        element.y = newBin;
                     }
                 }
-            } else {
-                if (y + size <= BOARD_SIZE) {
-                    for (let j = 1; j < size; j++) {
-                        newState[x][BOARD_SIZE - j].isOccupied = true;
-                        newState[x][BOARD_SIZE - j].id = id;
-                    }
-                } else {
-                    for (let j = 0; j < size; j++) {
-                        newState[x][y + j].isOccupied = true;
-                        newState[x][y + j].id = id;
-                    }
-                }
-            }
+            })
 
             return newState;
         })
-        */
     }
 
-    const [occupiedSpaces, setOccupiedSpaces] = useState(initializeBoardFromPuzzle(TEST_PUZZLE));
+    const [piecesOnBoard, setPiecesOnBoard] = useState(TEST_PUZZLE.pieces);
 
-    let pieces = populateBoardFromPuzzle(TEST_PUZZLE, occupiedSpaces);
+    useEffect(() => {setIsMounted(true)})
 
     return (
         <div id='main-board' className='board'>
-            {pieces}
+            {isMounted && populateBoardFromPuzzle(piecesOnBoard)}
         </div>
     )
 }
--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
 import Draggable from 'react-draggable';
 
-//const BOARD_PX = document.getElementById('main-board').getBoundingClientRect.width;
-//console.log(BOARD_PX);
 const BOARD_SIZE = 6;
 const SIZE_MOD = 100 / BOARD_SIZE;
+
+const calculateBounds = (id, x, y, h, w) => {
+    let boardBoundingRect = document.getElementById('main-board').getBoundingClientRect();
+    let tileSize = boardBoundingRect.width / BOARD_SIZE;
+
+    let left = parseInt(0 - (x * tileSize));
+    let top = parseInt(0 - (y * tileSize));
+    let right = parseInt(boardBoundingRect.width - w);
+    let bottom = parseInt(boardBoundingRect.height - h);
+    
+    return {
+        left: left, 
+        top: top, 
+        right: right, 
+        bottom: bottom
+    }
+}
 
 const preparePiece = (id, x, y, h, w, axis, color, coordHandler) => {
     let xPos = x * SIZE_MOD;
@@ -12,8 +27,11 @@ const preparePiece = (id, x, y, h, w, axis, color, coordHandler) => {
     let height = h * SIZE_MOD;
     let width = w * SIZE_MOD;
 
+    let bounds = calculateBounds(id, x, y, h, w)
+    console.log(bounds)
+
     return (
-        <Draggable axis={axis} bounds='parent' onStop={coordHandler}>
+        <Draggable axis={axis} bounds={calculateBounds(id, xPos, yPos, height, width)} onStop={coordHandler}>
             <div id={id} style={{
                 position:'absolute', 
                 left:`${xPos}%`, 
@@ -30,7 +48,7 @@ const Piece = props => {
     const [currX, setCurrX] = useState(props.pieceProps.x);
     const [currY, setCurrY] = useState(props.pieceProps.y);
 
-    const passNewCoordsToParent = () => {
+    const passNewBinToParent = () => {
         let currElement = document.getElementById(props.pieceProps.id)
 
         let boardPx = document.getElementById('main-board').getBoundingClientRect().width
@@ -65,8 +83,7 @@ const Piece = props => {
                     newBin = 0
             }
         
-            //setCurrX(newBin)
-            props.updateCoords(props.pieceProps.id, newBin, currY, props.pieceProps.orientation, props.pieceProps.size)
+            props.onFinishedDragging(props.pieceProps.id, newBin)
         } else {
             // Has transforms for both X and Y directions -- X will be 0
             let yTransform = Number(vals[1])
@@ -97,14 +114,13 @@ const Piece = props => {
                     newBin = 0
             }
             
-            //setCurrY(newBin)
-            props.updateCoords(props.pieceProps.id, currX, newBin, props.pieceProps.orientation, props.pieceProps.size)
+            props.onFinishedDragging(props.pieceProps.id, newBin)
         }
     }
 
     let currPiece = props.pieceProps.orientation == 'HORIZONTAL' 
-        ? preparePiece(props.pieceProps.id, currX, currY, 1, props.pieceProps.size, 'x', props.pieceProps.color, passNewCoordsToParent)
-        : preparePiece(props.pieceProps.id, currX, currY, props.pieceProps.size, 1, 'y', props.pieceProps.color, passNewCoordsToParent);
+        ? preparePiece(props.pieceProps.id, currX, currY, 1, props.pieceProps.size, 'x', props.pieceProps.color, passNewBinToParent)
+        : preparePiece(props.pieceProps.id, currX, currY, props.pieceProps.size, 1, 'y', props.pieceProps.color, passNewBinToParent);
 
     return currPiece;
 }
